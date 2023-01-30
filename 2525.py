681f1adb892913fbe7b1e3232a34470b154e522a
xoxowo <1qq11212@gmail.com>
2023-01-30 14:24:42 +0900

    2525 푸는중

---
+++ 2525.py
@@ -0,0 +1,20 @@
+a,b = map(int, input().split())
+c = int(input())
+
+print( (b+c)//60)
+if a==23 :
+    a = 0
+
+
+# if b+c < 60 and b==0:
+#     print(a, b+c)
+# elif b+c >= 60:
+#     a = a + (b+c)//60
+#     b = (b+c)-60
+#     print(a,b)
+#     if a==23 :
+#         a= 0 + (b+c)//60
+#         b = (b+c)-60
+#         print(a,b)
+    
+
