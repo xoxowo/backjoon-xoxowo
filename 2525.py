b82e6471bed924d2ceda2052285a1b1948abe2cf
xoxowo <1qq11212@gmail.com>
2023-01-31 14:46:50 +0900

    2884 품..

--- 2525.py
+++ 2525.py
@@ -1,9 +1,18 @@
-a,b = map(int, input().split())
+h,m = map(int, input().split())
 c = int(input())
 
-print( (b+c)//60)
-if a==23 :
-    a = 0
+# if h>0 and m+c>60 :
+#     print()
+if m+c <60:
+    print(h, m+c)
+elif h == 23 and m+c > 60:
+    m = (m+c)-60
+    h = ((m+c)//60)
+    print(h, m)
+    
+    
+else :
+    print(h+1,  )
 
 
 # if b+c < 60 and b==0:
@@ -18,3 +27,7 @@ if a==23 :
 #         print(a,b)
     
 
+# 현재 시간에서 +m 만큼 ++
+# m+c가 60을 안넘으면 h,m+c
+# m+c가 60보다 크면 h+1, (m+c)-60
+# 근데 h가 ==23이다 그러면 h는 0이되어야하는데 여기서 m+c가 // 1이면 0
--- 2884(못품).py
+++
@@ -1,7 +0,0 @@
-h,m = map(int, input().split())
-
-if h >0 and m > 0 :
-    a= h-1
-    b= m-45  
-
-    print(a, )
