c7c1bc5a7790c871e0416507272061bd11589880
xoxowo <1qq11212@gmail.com>
2023-02-02 13:50:36 +0900

    11021풀이

---
+++ level_3/11021.py
@@ -0,0 +1,27 @@
+"""
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. 
+(0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 
+테스트 케이스 번호는 1부터 시작한다.    
+"""
+
+import sys
+
+t = int(input())
+# s = [list(map(int, sys.stdin.readline().split()))for _ in range(t)]
+
+# for i in range(t):
+#     for j in s :
+#         print("Case ",1+j,": ",(j[0]+j[1]))
+#     break
+
+for i in range(t):
+    a,b = map(int, input().split())
+    print("Case #"+str(i+1)+":",(a+b))
