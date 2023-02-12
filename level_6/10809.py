2ffea4b9d8a6e85592e23dc3ca37318da1c58f77
xoxowo <1qq11212@gmail.com>
2023-02-12 23:54:31 +0900

    1152 수정

--- level_6/10809.py
+++ level_6/10809.py
@@ -14,7 +14,7 @@
 만약, 어떤 알파벳이 단어에 포함되어 있지 않다면 -1을 출력한다. 
 단어의 첫 번째 글자는 0번째 위치이고, 두 번째 글자는 1번째 위치이다.
 """
-
+# find 함수사용하면 됨? 
 
 s= list(map(ord,input()))
 print(s)
@@ -23,4 +23,3 @@ for i in range(len(s)):
     for j in range(97, 122):
         if j in s :
             
-
